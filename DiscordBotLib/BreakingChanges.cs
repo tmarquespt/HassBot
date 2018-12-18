229bc2069a7b55ac479a293704fdb6805b453077
skalavala <mahasri.kalavala@gmail.com>
2018-12-17 21:00:46 -0500

    merge fix

--- DiscordBotLib/BreakingChanges.cs
+++ DiscordBotLib/BreakingChanges.cs
@@ -39,7 +39,6 @@ namespace DiscordBotLib
                 await ReplyAsync(string.Empty, false, embed);
                 return;
             }
-<<<<<<< HEAD
             else
             {
                 if ( url.EndsWith("/"))
@@ -49,48 +48,6 @@ namespace DiscordBotLib
             }
 
             await ReplyAsync("<" + url + ">" );
-=======
-
-            StringBuilder sb = new StringBuilder();
-            string data = HassBotData.Helper.DownloadBreakingChanges(url);
-
-            string bc = GetValue(data, "</a> Breaking Changes</h2>\n\n<ul>\n  ", "</ul>");
-            string[] lines = bc.Split('\n');
-            if (lines.Count() > 0)
-                sb.Append("Here are the breaking changes in version: ." + version + ".0\n```");
-
-            foreach (string line in lines)
-            {
-                string tmp = line.Trim();
-                if (tmp == string.Empty)
-                    continue;
-
-                string pr = GetValue(tmp, "home-assistant/home-assistant/pull/", "\"");
-                string component = GetValue(tmp, "(<a href=\"/components/", "/\">");
-                string description = GetValue(tmp, "<li>", "(<a ");
-                //string docLink = string.Format("https://www.home-assistant.io/components/{0}", component);
-
-                sb.Append(string.Format("PR# {0}- [{1}] {2}", pr, component.ToUpper(), description));
-                sb.Append("\n");
-            }
-            sb.Append("```");
-            await ReplyAsync(sb.ToString());
-        }
-
-        private string GetValue(string line, string start_tag, string end_tag)
-        {
-            int startIndex = line.IndexOf(start_tag);
-            if (startIndex == -1)
-                return string.Empty;
-
-            startIndex = startIndex + start_tag.Length;
-            int endIndex = line.IndexOf(end_tag, startIndex);
-            if (endIndex == -1)
-                return string.Empty;
-
-            string value = line.Substring(startIndex, (endIndex - startIndex));
-            return value;
->>>>>>> 80df38434060d8709f2881d733e5e709ad4c49a6
         }
     }
 }
