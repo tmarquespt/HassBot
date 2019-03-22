182507d8d36f24b6724d6993dc998327d23d9e7c
vMeph <vMeph@users.noreply.github.com>
2019-03-22 20:09:41 +0000

    Update FormatModule.cs

--- DiscordBotLib/FormatModule.cs
+++ DiscordBotLib/FormatModule.cs
@@ -34,7 +34,7 @@ namespace DiscordBotLib
 
             sb.Append("Para formatar o teu texto como código, insira três backticks na primeira linha, pressione Enter para uma nova linha, cole seu código, pressione Enter novamente para outra nova linha e, por último, três outros backticks. Aqui está um exemplo:\n\n");
             sb.Append("\\`\\`\\`\n");
-            sb.Append("code here\n");
+            sb.Append("código aqui\n");
             sb.Append("\\`\\`\\`\n");
             sb.Append("Watch the animated gif here: <https://bit.ly/2GbfRJE>\n");
             sb.Append("**DO NOT** repeat posts. Please edit previously posted message, here is how -> <https://bit.ly/2qOOf1G>");
