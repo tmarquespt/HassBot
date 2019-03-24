6652a342d01a22c9e5355d7da78512a2840ece6e
Tiago Marques <38387300+tmarquespt@users.noreply.github.com>
2019-03-24 10:25:12 +0000

    Update AboutModule.cs

--- DiscordBotLib/AboutModule.cs
+++ DiscordBotLib/AboutModule.cs
@@ -33,7 +33,6 @@ namespace DiscordBotLib
         private async Task AboutCommand() {
             var embed = new EmbedBuilder();
             embed.WithTitle("Bem-vindo! Eu sou @CPHAbot \n");
-            embed.AddField ("Se tiverem alguma ideias para o meu desenvolvimento contactem @Tiago \n");
             embed.WithColor(Helper.GetRandomColor());
             embed.AddInlineField("Ligado desde", $"{ GetUptime() }");
             embed.AddInlineField("Total de usuários", $"{Context.Client.Guilds.Sum(g => g.Users.Count)}");
