584cb328afefd7e583d8c259f8fb36665591876e
vMeph <arduino328p@gmail.com>
2019-03-29 19:28:18 +0000

    Update FormatModule.cs

--- DiscordBotLib/FormatModule.cs
+++ DiscordBotLib/FormatModule.cs
@@ -33,7 +33,7 @@ namespace DiscordBotLib
                 sb.Append(mentionedUsers + " ");
 
             sb.Append("Para formatar o teu texto como código, insere três assim:  ``` na primeira linha, pressiona Enter para uma nova linha, cole seu código, pressione Enter novamente para outra nova linha e, por último, três outros backticks. Aqui está um exemplo:\n\n");
-            sb.Append("\\`\\`\\`yaml\n");
+            sb.Append("\\`\\`\\`\\yaml\n");
             sb.Append("código aqui\n");
             sb.Append("\\`\\`\\`\n");
             sb.Append("Vê o gif animado aqui: <https://bit.ly/2GbfRJE>\n");
