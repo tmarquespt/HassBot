580b05103f5d680793ca425c84be6aa4e378e4ee
Jorge Assunção <jorgeassuncao@users.noreply.github.com>
2019-04-04 14:47:32 +0100

    Tradução para pt-PT e alteração dos links para TinyURL

--- DiscordBotLib/FormatModule.cs
+++ DiscordBotLib/FormatModule.cs
@@ -32,13 +32,15 @@ namespace DiscordBotLib
             if (mentionedUsers.Trim() != string.Empty )
                 sb.Append(mentionedUsers + " ");
 
-            sb.Append("Para formatares o teu texto como código insere três acentos graves na primeira linha, seguido de 'yaml', pressiona Enter para uma nova linha, cola o teu código, pressiona Enter novamente para outra nova linha e, por último, outros três acentos graves. Aqui está um exemplo:\n\n");
+            sb.Append("Para formatares o teu texto como código insere três acentos graves na primeira linha, seguido de 'yaml', pressiona Enter para uma nova linha, cola o teu código, pressiona Enter novamente para outra nova linha e, por último, outros três acentos graves.");
+            sb.Append("Deverá ficar como neste exemplo:\n");
             sb.Append("\\`\\`\\`yaml \n");
-            sb.Append("código aqui\n");
+            sb.Append("código\n");
             sb.Append("\\`\\`\\`\n");
-            sb.Append("Vê o gif animado aqui: <https://bit.ly/2GbfRJE>\n");
             sb.Append(" ");
-            sb.Append("** NÃO ** repitas as mensagens. Por favor, edita a mensagem publicada anteriormente. Para saberes como o fazer vê aqui: <http://tiny.cc/kk3k4y>");
+            sb.Append("Vê o gif animado de como deves formatar o texto como código: <http://tiny.cc/1nos4y>\n");
+            sb.Append(" ");
+            sb.Append("Por favor, **NÃO** repitas as mensagens e edita a mensagem publicada anteriormente. Para saberes como o fazer vê aqui: <http://tiny.cc/kk3k4y>");
 
             await ReplyAsync(sb.ToString(), false, null);
         }
